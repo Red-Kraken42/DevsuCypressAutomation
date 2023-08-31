82f1ab1c60f4c4e9a10bf4c70e454fcbca0230e1
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-31 09:57:12 -0600

    Constants.js file

---
+++ cypress/support/Constants.js
@@ -0,0 +1,16 @@
+class Constants {
+  static INVENTORY_HTML = '/inventory.html'
+  static HAVE_TEXT = 'have.text'
+  static BUTTON = "button"
+  static CHECKOUT = "Checkout"
+  static RICARDO = "Ricardo"
+  static OSEGUEDA = "Osegueda"
+  static CP_1502 = "CP 1502"
+  static FINISH = "Finish"
+  static productNames = [
+    "Sauce Labs Backpack",
+    "Sauce Labs Onesie",
+  ];
+}
+
+module.exports = { Constants };
