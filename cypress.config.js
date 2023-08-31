33f88eddb0c59e3dfdd1b47724b153b49c3cfc78
Red-Kraken42 <red-kraken@hotmail.com>
2023-08-31 13:03:39 -0600

    improve report configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  "reporter": "mochawesome",
-  "reporterOptions": {
-    "reportDir": "cypress/results",
-    "overwrite": true,
-    "html": true,
-    "json": true
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Course Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
   },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
